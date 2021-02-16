fbe9bdb7859a8e452baf89ac7db68caddd1c2e0a
rohanarote <rohanarote7@gmail.com>
2021-02-16 23:51:40 +0530

    First Commit GirryaRohan

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
 require("dotenv").config();
-const connectionURL = "" + process.env.DATABASE;
+const connectionURL = "" + process.env.DATABASE + "";
 // app
 const app = express();
 console.log(connectionURL);
