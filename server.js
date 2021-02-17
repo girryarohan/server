a8b98038bf8d029cde4c45b3a4142eaf25d80f19
rohanarote <rohanarote7@gmail.com>
2021-02-18 00:11:53 +0530

    Updated db connection URL

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
 require("dotenv").config();
-// const path = require("path");
-// require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
-const connectionURL = "mongodb://localhost:27017/ecom";
+
 // app
 const app = express();
-console.log(connectionURL);
+
 // db
 mongoose
   .connect(process.env.DATABASE, {
